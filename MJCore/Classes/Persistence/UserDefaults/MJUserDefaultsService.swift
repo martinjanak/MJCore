ab65292a5b46201d6a96786d4afa8eef2d3c0da0
Martin Janák <janak.martin@email.cz>
2018-05-21 20:02:41 +0200

    Adjusting user defaults.

--- MJCore/Classes/Persistence/UserDefaults/MJUserDefaultsService.swift
+++ MJCore/Classes/Persistence/UserDefaults/MJUserDefaultsService.swift
@@ -9,30 +9,30 @@ import Foundation
 
 public protocol MJUserDefaultsService {
     associatedtype KeyType: RawRepresentable where KeyType.RawValue == String
-    func set<T>(key: KeyType, value: T)
-    func get<T>(key: KeyType) -> T?
-    func get<T>(key: KeyType, defaultValue: T) -> T
-    func delete(key: KeyType)
+    func set<T>(_ key: KeyType, value: T)
+    func get<T>(_ key: KeyType) -> T?
+    func get<T>(_ key: KeyType, default: T) -> T
+    func delete(_ key: KeyType)
 }
 
 extension MJUserDefaultsService {
     
-    public func set<T>(key: KeyType, value: T) {
+    public func set<T>(_ key: KeyType, value: T) {
         UserDefaults.standard.set(value, forKey: key.rawValue)
     }
     
-    public func get<T>(key: KeyType) -> T? {
+    public func get<T>(_ key: KeyType) -> T? {
         return UserDefaults.standard.object(forKey: key.rawValue) as? T
     }
     
-    public func get<T>(key: KeyType, defaultValue: T) -> T {
-        if let object: T = get(key: key) {
+    public func get<T>(_ key: KeyType, default: T) -> T {
+        if let object: T = get(key) {
             return object
         }
-        return defaultValue
+        return `default`
     }
     
-    public func delete(key: KeyType) {
+    public func delete(_ key: KeyType) {
         UserDefaults.standard.removeObject(forKey: key.rawValue)
     }
 }
