9b26ee4d421eeda77de3a4b23edf71c2f6547674
Martin Janák <janak.martin@email.cz>
2018-05-26 20:52:22 +0200

    Adding poller.

---
+++ MJCore/Classes/Rx/MJPoller.swift
@@ -0,0 +1,82 @@
+//
+//  MJPoller.swift
+//  MJCore
+//
+//  Created by Martin Janák on 24/05/2018.
+//
+
+import RxSwift
+
+public enum MJPollerError: Error {
+    case timeout
+}
+
+public final class MJPoller {
+    
+    private let tickSubject = PublishSubject<MJResult<Double>>()
+    public lazy var tick = tickSubject.asObservable()
+    
+    private let queue = DispatchQueue(
+        label: "Poller",
+        qos: .background,
+        attributes: .concurrent
+    )
+    
+    private var timer: Timer?
+    private let interval: Double
+    private var seconds: Double = 0
+    private let timeout: Double?
+    
+    public init(interval: Double, timeout: Double? = nil) {
+        self.interval = interval
+        self.timeout = timeout
+    }
+    
+    public func start() {
+        queue.async {
+            self.stopSync()
+            self.timer = Timer(
+                timeInterval: self.interval,
+                target: self,
+                selector: #selector(self.tickHandler),
+                userInfo: nil,
+                repeats: true
+            )
+            RunLoop.current.add(
+                self.timer!,
+                forMode: RunLoopMode.defaultRunLoopMode
+            )
+            RunLoop.current.run()
+        }
+    }
+    
+    public func stop() {
+        queue.async {
+            self.stopSync()
+        }
+    }
+    
+    private func stopSync() {
+        timer?.invalidate()
+        timer = nil
+        seconds = 0
+    }
+    
+    @objc
+    private func tickHandler() {
+        if let timeout = timeout, timeout <= seconds {
+            tickSubject.onNext(.failure(error: MJPollerError.timeout))
+            stop()
+        } else {
+            tickSubject.onNext(.success(value: seconds))
+            seconds = seconds + interval
+        }
+    }
+    
+    deinit {
+        queue.sync {
+            stopSync()
+        }
+    }
+    
+}
