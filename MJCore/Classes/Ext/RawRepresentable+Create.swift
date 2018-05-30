133dd6a7056212359919f2e32745ef3243e29662
Martin Janák <janak.martin@email.cz>
2018-05-30 20:22:14 +0200

    String raw representable error.

--- MJCore/Classes/Ext/RawRepresentable+Create.swift
+++ MJCore/Classes/Ext/RawRepresentable+Create.swift
@@ -8,15 +8,15 @@
 import Foundation
 
 public enum MJEnumError: Error {
-    case rawValueMismatch
+    case rawValueMismatch(value: String)
 }
 
-extension RawRepresentable {
+extension RawRepresentable where RawValue == String {
     public static func create(_ rawValue: RawValue) throws -> Self {
         if let value = Self.init(rawValue: rawValue) {
             return value
         } else {
-            throw MJEnumError.rawValueMismatch
+            throw MJEnumError.rawValueMismatch(value: rawValue)
         }
     }
 }
