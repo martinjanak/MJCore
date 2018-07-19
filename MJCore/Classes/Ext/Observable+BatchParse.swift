a12a61f2d33dc2e085946f1b2bfe82ec67992cf9
Martin Janák <janak.martin@email.cz>
2018-07-19 08:11:05 +0200

    Batch parse array.

--- MJCore/Classes/Ext/Observable+BatchParse.swift
+++ MJCore/Classes/Ext/Observable+BatchParse.swift
@@ -9,7 +9,7 @@ import RxSwift
 
 extension Observable where Element == MJResult<[Data]>  {
     
-    public func parse<Model: MJsonParsable>(
+    public func batchParse<Model: MJsonParsable>(
         _ modelType: Model.Type,
         key: String? = nil,
         defaults: MJson? = nil
@@ -25,6 +25,35 @@ extension Observable where Element == MJResult<[Data]>  {
                         key: key,
                         defaults: defaults
                     ))
+                } catch let error {
+                    allParseSuccess = false
+                    message.append("{\(error)} ")
+                }
+            }
+            if allParseSuccess {
+                return models
+            } else {
+                throw MJObservableError.batch(message: message)
+            }
+        }
+    }
+    
+    public func batchParse<Model: MJsonParsable>(
+        _ modelType: [Model].Type,
+        key: String? = nil,
+        defaults: MJson? = nil
+    ) -> Observable<MJResult<[[Model]]>> {
+        return self.successMap { dataArray -> [[Model]] in
+            var allParseSuccess = true
+            var message = ""
+            var models = [[Model]]()
+            for data in dataArray {
+                do {
+                    models.append(try MJson.parseArrayModel(
+                        data,
+                        key: key,
+                        defaults: defaults
+                        ))
                 } catch let error {
                     allParseSuccess = false
                     message.append("{\(error)} ")
