a8f75cea054fceadc1c778386646ed2c7eb1fd90
Martin Janák <janak.martin@email.cz>
2018-07-24 17:15:15 +0200

    Resizing collection view.

---
+++ MJCore/Classes/View/CollectionView/MJResizingCollectionView.swift
@@ -0,0 +1,18 @@
+//
+//  MJAutoresizingCollectionView.swift
+//  MJCore
+//
+//  Created by Martin Janák on 24/07/2018.
+//
+
+import UIKit
+import Stevia
+
+open class AXResizingCollectionView: UICollectionView {
+    
+    override open func layoutSubviews() {
+        super.layoutSubviews()
+        heightConstraint?.constant = collectionViewLayout.collectionViewContentSize.height
+    }
+    
+}
