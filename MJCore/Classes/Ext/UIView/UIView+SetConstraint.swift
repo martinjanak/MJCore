e6d7372658934c7637ca19553de1c4bc19f89e77
Martin Janák <janak.martin@email.cz>
2018-10-30 18:44:13 +0100

    Set constraint.

---
+++ MJCore/Classes/Ext/UIView/UIView+SetConstraint.swift
@@ -0,0 +1,49 @@
+//
+//  UIView.swift
+//  MJCore
+//
+//  Created by Martin Janák on 30/10/2018.
+//
+
+import UIKit
+import Stevia
+
+extension UIView {
+    
+    public func setTop(_ constant: CGFloat) -> UIView {
+        if let topConstraint = self.topConstraint {
+            topConstraint.constant = constant
+            return self
+        } else {
+            return top(constant)
+        }
+    }
+    
+    public func setBottom(_ constant: CGFloat) -> UIView {
+        if let bottomConstraint = self.bottomConstraint {
+            bottomConstraint.constant = constant
+            return self
+        } else {
+            return bottom(constant)
+        }
+    }
+    
+    public func setLeft(_ constant: CGFloat) -> UIView {
+        if let leftConstraint = self.leftConstraint {
+            leftConstraint.constant = constant
+            return self
+        } else {
+            return left(constant)
+        }
+    }
+    
+    public func setRight(_ constant: CGFloat) -> UIView {
+        if let rightConstraint = self.rightConstraint {
+            rightConstraint.constant = constant
+            return self
+        } else {
+            return right(constant)
+        }
+    }
+    
+}
