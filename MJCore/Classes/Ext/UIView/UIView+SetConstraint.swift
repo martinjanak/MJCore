c867815b9127dbda201bf01bdac8249c9376acb9
Martin Janák <janak.martin@email.cz>
2018-10-30 18:48:31 +0100

    Adjusting set constraint.

--- MJCore/Classes/Ext/UIView/UIView+SetConstraint.swift
+++ MJCore/Classes/Ext/UIView/UIView+SetConstraint.swift
@@ -10,6 +10,7 @@ import Stevia
 
 extension UIView {
     
+    @discardableResult
     public func setTop(_ constant: CGFloat) -> UIView {
         if let topConstraint = self.topConstraint {
             topConstraint.constant = constant
@@ -19,6 +20,7 @@ extension UIView {
         }
     }
     
+    @discardableResult
     public func setBottom(_ constant: CGFloat) -> UIView {
         if let bottomConstraint = self.bottomConstraint {
             bottomConstraint.constant = constant
@@ -28,6 +30,7 @@ extension UIView {
         }
     }
     
+    @discardableResult
     public func setLeft(_ constant: CGFloat) -> UIView {
         if let leftConstraint = self.leftConstraint {
             leftConstraint.constant = constant
@@ -37,12 +40,33 @@ extension UIView {
         }
     }
     
+    @discardableResult
     public func setRight(_ constant: CGFloat) -> UIView {
         if let rightConstraint = self.rightConstraint {
             rightConstraint.constant = constant
             return self
         } else {
             return right(constant)
+        }
+    }
+    
+    @discardableResult
+    public func setHeight(_ constant: CGFloat) -> UIView {
+        if let heightConstraint = self.heightConstraint {
+            heightConstraint.constant = constant
+            return self
+        } else {
+            return height(constant)
+        }
+    }
+    
+    @discardableResult
+    public func setWidth(_ constant: CGFloat) -> UIView {
+        if let widthConstraint = self.widthConstraint {
+            widthConstraint.constant = constant
+            return self
+        } else {
+            return width(constant)
         }
     }
     
