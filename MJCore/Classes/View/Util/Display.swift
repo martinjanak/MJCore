8286933da9a4ae0354fb17073528b4d6c6da34e0
Martin Janák <janak.martin@email.cz>
2018-11-01 08:32:11 +0100

    Adjusting display util.

--- MJCore/Classes/View/Util/Display.swift
+++ MJCore/Classes/View/Util/Display.swift
@@ -9,11 +9,11 @@ import Foundation
 
 public final class Display {
     
-    class var hasTopNotch: Bool {
+    public static var hasTopNotch: Bool {
         return (topSafeArea ?? 0) > 20
     }
     
-    class var topSafeArea: CGFloat? {
+    public static var topSafeArea: CGFloat? {
         if #available(iOS 11.0, *) {
             // with notch: 44.0 on iPhone X, XS, XS Max, XR.
             // without notch: 20.0 on iPhone 8 on iOS 12+.
@@ -22,7 +22,7 @@ public final class Display {
         return nil
     }
     
-    class var bottomSafeArea: CGFloat? {
+    public static var bottomSafeArea: CGFloat? {
         if #available(iOS 11.0, tvOS 11.0, *) {
             // with home indicator: 34.0 on iPhone X, XS, XS Max, XR.
             return UIApplication.shared.delegate?.window??.safeAreaInsets.bottom
