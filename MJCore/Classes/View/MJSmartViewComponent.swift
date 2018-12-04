bb9d6f78f9de81ecd1a42a11e5f7e96a6de445ff
Martin Janák <janak.martin@email.cz>
2018-12-04 12:10:25 +0100

    Reintroducing smart view component.

---
+++ MJCore/Classes/View/MJSmartViewComponent.swift
@@ -0,0 +1,17 @@
+//
+//  MJSmartViewComponent.swift
+//  MJCore
+//
+//  Created by Martin Janák on 04/12/2018.
+//
+
+import UIKit
+import RxSwift
+
+open class MJSmartViewComponent<ViewModel: MJViewModel>: MJViewComponent {
+    
+    open func initBindings(viewModel: ViewModel) {
+        // override
+    }
+    
+}
